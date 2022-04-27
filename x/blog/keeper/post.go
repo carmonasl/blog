f104bdb224045a19fdb1b25d88469b7e15480883
carmonasl <alberto.carmona.sanchez@gmail.com>
2022-04-27 23:06:40 +0200

    Fixed repo username

--- x/blog/keeper/post.go
+++ x/blog/keeper/post.go
@@ -2,7 +2,7 @@ package keeper
 
 import (
   "encoding/binary"
-  "github.com/username/blog/x/blog/types"
+  "github.com/carmonasl/blog/x/blog/types"
   "github.com/cosmos/cosmos-sdk/store/prefix"
   sdk "github.com/cosmos/cosmos-sdk/types"
 )
